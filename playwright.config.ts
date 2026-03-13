0f716674abf52261f932248e423b13fa32c52db4
Gonzalo <gbaldiviezo@revaicare.com>
2026-03-13 20:19:18 +1100

    Added config for screenshots, video and trace for tests on failure

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,11 +25,15 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    headless: true,
+
+    screenshot: 'only-on-failure',
+
+    video: 'retain-on-failure',
+
+    trace: 'retain-on-failure',
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
   },
 
   /* Configure projects for major browsers */
